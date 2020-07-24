82bc1cb788fc1582944aa9566d9b333103758209
ahtohlove <ahtohlove@163.com>
2020-07-24 16:01:27 +0800

    init repo

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author ahtoh
+ * @create 2020-07-24 15:52
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
